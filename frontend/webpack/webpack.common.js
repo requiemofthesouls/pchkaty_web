bbcbe71b7c3b65fc7f506e02b59eebf30fe28cbe
keep_on_rail <dimamid99@gmail.com>
2020-02-07 22:25:46 +0500

    webpack configuration

---
+++ frontend/webpack/webpack.common.js
@@ -0,0 +1,57 @@
+// shared config (dev and prod)
+const {resolve} = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    context: resolve(__dirname, '../src'),
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: 'style-loader',
+                }, {
+                    loader: 'css-loader', // translates CSS into CommonJS
+                }, {
+                    loader: 'less-loader', // compiles Less to CSS
+                    options: {
+                        modifyVars: {
+                            'border-radius-base': '0px',
+                        },
+                        javascriptEnabled: true,
+                    },
+                }],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]',
+                    'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: 'public/index.html',}),
+    ],
+    externals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM',
+    },
+    performance: {
+        hints: false,
+    },
+};
